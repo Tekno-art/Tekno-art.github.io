2b427f96c1efd72d57426c7a2def23ddc5b7c947
panjiafridio <panjiafridio321@gmail.com>
2020-11-05 13:55:25 +0700

    Sudah Selesai Tinggal media query dan beberapa perbaikan

--- Content page/script.js
+++ Content page/script.js
@@ -1,6 +1,13 @@
 const spaceImg = document.querySelectorAll('.space-img');
+const shadow = document.querySelector('.shadow');
 
 window.addEventListener('scroll', () =>{
     let scroll = window.pageYOffset;
-    console.log(scroll);
-});
+    
+    spaceImg.forEach(e => {
+        let speed = e.dataset.speed;
+        e.style.transform = `translateY(${scroll * speed}px)`
+    })
+    shadow.style.height = `${scroll * 0.5 + 200}px`;
+})
+
