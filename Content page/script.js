d5fe39f8b2d8b7d11b5056758ee2a89198c8b2cc
panjiafridio <panjiafridio321@gmail.com>
2020-11-08 16:10:38 +0700

    Kurang animasi hidden navbar

--- Content page/script.js
+++ Content page/script.js
@@ -11,3 +11,38 @@ window.addEventListener('scroll', () =>{
     shadow.style.height = `${scroll * 0.5 + 200}px`;
 })
 
+const bars = document.querySelector(".bars");
+const navbar = document.querySelector(".navbar1");
+const close = document.querySelector(".close");
+const judul = document.querySelector(".judul");
+
+
+bars.addEventListener('click', () =>{
+    anime({
+        targets : '.navbar1',
+        translateY : 0,
+        duration : 800,
+        easing : 'linear' 
+    })
+    anime({
+        targets : '.judul',
+        opacity : 0,
+        duration : 500,
+        easing : 'linear'
+    })
+})
+close.addEventListener('click', () =>{
+    anime({
+        targets : '.navbar1',
+        translateY : -250,
+        duration : 800,
+        easing : 'linear' 
+    })
+    anime({
+        targets : '.judul',
+        opacity : 1,
+        duration : 500,
+        easing : 'linear'
+    })
+})
+
